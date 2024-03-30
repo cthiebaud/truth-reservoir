220ae661bfb981c479bda7b452b7ab516da9f9ac
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-03-31 00:32:23 +0100

    cleaned

--- dist/.platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d truth-reservoir-env.eba-3iuiqiva.eu-north-1.elasticbeanstalk.com --nginx --agree-tos --email christophe.t60@gmail.com
--- dist/zip_dist.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-zip -r ../dist.zip truth-reservoir-1.0-SNAPSHOT.jar .ebextensions .platform
